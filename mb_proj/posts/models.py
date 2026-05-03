371a08eafb9e29db4659079f144465da07a50280
Ashley K Motsie <motsieashley31@gmail.com>
2026-05-03 19:31:11 +0200

    fix: models fix and registry

--- mb_proj/posts/models.py
+++ mb_proj/posts/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.utils.timezone import now
 
 class Post(models.Model):
+    title = models.CharField(max_length=200)
     text = models.TextField()
+    created_date = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return self.title
